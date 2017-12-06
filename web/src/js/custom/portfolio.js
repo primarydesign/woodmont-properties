23a98ed202b33997c2e88e595b930c3689f67a80
Brian Hogan, Ph.D <brian@primarydesign.com>
2017-12-06 12:00:28 -0500

    Parallax inclusion, init in portfolio.js

--- web/src/js/custom/portfolio.js
+++ web/src/js/custom/portfolio.js
@@ -92,3 +92,6 @@ $(function() {
 $('body').on('hidden.bs.modal', '.modal', function() {
     $('video').trigger('pause');
 });
+//parallax
+$('.section__header, .section__hero').parallax({imageSrc: '/app/themes/woodmont-props-child/assets/img/header-image_properties.jpg', zIndex: 1});
+$('.section__header h2, .section__hero h1').css({'z-index': 2, 'position': 'relative'});
